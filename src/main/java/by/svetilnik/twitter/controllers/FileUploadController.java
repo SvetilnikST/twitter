c0d350b6c804bf16a9f1ed699e31d314d5b57052
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-02-10 16:55:56 +0300

    uploadFiles

---
+++ src/main/java/by/svetilnik/twitter/controllers/FileUploadController.java
@@ -0,0 +1,11 @@
+package by.svetilnik.twitter.controllers;
+
+import by.svetilnik.twitter.storage.StorageService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FileUploadController {
+
+//    private final StorageService storageService;
+
+}
---
+++ src/main/java/by/svetilnik/twitter/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package by.svetilnik.twitter.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
